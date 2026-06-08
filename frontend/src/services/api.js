add4c3b33ef995506f506bf92c65c24deeb808ca
yadavsriyansh400-ux <yadavsriyansh400@gmail.com>
2026-06-09 02:58:28 +0530

    fixed api issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://junglechat-backend.onrender.com",
+  baseURL: "https://junglechat-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
